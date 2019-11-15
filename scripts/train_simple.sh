bdd85aa4824b7a4e7821fe6fbec71ec80d6a9dcc
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-15 17:49:31 +0800

    change api for new BERT model

--- scripts/train_simple.sh
+++ scripts/train_simple.sh
@@ -8,7 +8,7 @@ BERT_OUTPUT_PATH=/data/zhuoyu/summarization/workspace/run_training_2
 mkdir $BERT_OUTPUT_PATH
 mkdir $BERT_OUTPUT_PATH/logs
 
-CUDA_VISIBLE_DEVICES=3,4,5 python train.py -task ext -mode train -bert_data_path $BERT_DATA_PATH \
+python train.py -task ext -mode train -bert_data_path $BERT_DATA_PATH \
 -ext_dropout 0.1 -model_path $BERT_OUTPUT_PATH \
 -lr 2e-3 -visible_gpus 3,4,5 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -train_steps 50000 -accum_count 2 \
--log_file $BERT_OUTPUT_PATH/logs/ext_bert_cnndm -use_interval true -warmup_steps 10000 -max_pos 512
+-log_file $BERT_OUTPUT_PATH/logs/ext_bert_cnndm -use_interval true -warmup_steps 10000 -max_pos 512 -dist_init_method "tcp://localhost:10003"
