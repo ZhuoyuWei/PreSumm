b36f166aa211a8dd3af6cbe990c9e89591fbcc5c
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-20 10:03:07 +0800

    fix bug in mclstm 1120.1

--- src/transformers/modeling_bert_mclstm.py
+++ src/transformers/modeling_bert_mclstm.py
@@ -371,7 +371,7 @@ class GlobalMCLSTMLayer(nn.Module):
         lstm_output, (hn, cn)=self.lstm(hidden_states_t.transpose(0,1)) #lstm_output = c * bl * 2d
         lstm_output=lstm_output.transpose(0,1).view(ori_size[0],ori_size[2],ori_size[1],ori_size[3],2).sum(dim=-1).squeeze(-1) #lstm_output = b * l * c * d
         lstm_output=lstm_output.transpose(1,2) #b * c * l * d
-        outputs=self.output(lstm_output.view(ori_size[0],ori_size[1]*ori_size[2],ori_size[3])
+        outputs=self.output(lstm_output.view(ori_size[0],ori_size[1]*ori_size[2],ori_size[3].contiguous())
                             ,hidden_states.view(ori_size[0],ori_size[1]*ori_size[2],ori_size[3]))
         print('debug output size = {}'.format(outputs.size()))
         return outputs
