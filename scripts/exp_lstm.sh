b4b2622255196e0edc093636b78881a19f1c261a
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-22 17:31:54 +0800

    delay only mc baseline

--- scripts/exp_lstm.sh
+++ scripts/exp_lstm.sh
@@ -11,7 +11,7 @@ mkdir $BERT_OUTPUT_PATH/logs
 
 python train.py -task ext -mode train -bert_data_path $BERT_DATA_PATH \
 -ext_dropout 0.1 -model_path $BERT_OUTPUT_PATH \
--lr 2e-3 -visible_gpus 0,1,2,3 -report_every 50 -save_checkpoint_steps 2000 -batch_size 1000 -train_steps 50000 -accum_count 2 \
+-lr 2e-3 -visible_gpus 0,1,2,3,4,5,6,7 -report_every 50 -save_checkpoint_steps 2000 -batch_size 1000 -train_steps 50000 -accum_count 2 \
 -log_file $BERT_OUTPUT_PATH/logs/ext_bert_cnndm -use_interval true -warmup_steps 10000 -max_pos 512 -dist_init_method "tcp://localhost:10002" \
 -model_name bert_lstm -pretrained_name $PRETRAINED_PATH -max_model_pos=128 -gobal_layer_type=lstm
 
