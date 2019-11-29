79a6dca2f60879a200cc51581e5f55dbbc97caba
zhuwe <zhuwe@microsoft.com>
2019-11-29 23:16:41 +0800

    add train_valid

--- scripts/train_valid.sh
+++ scripts/train_valid.sh
@@ -6,6 +6,10 @@ EXP_MODEL_NAME=$2
 MAX_POS=$3
 MAX_MODEL_POS=$4
 
+ZONG_MODEL=$5 #bert, bert_lstm
+ENCODER_TYPE=$6  #'bert', 'baseline'
+
+
 WDATA_DIR=/data/zhuoyu/summarization/wdata
 OUTPUT_DIR=$WDATA_DIR/$EXP_ID
 mkdir $OUTPUT_DIR
@@ -51,7 +55,11 @@ MODEL_DIR=$EXP_ROOT_DIR/models
 mkdir $MODEL_DIR
 cp /data/zhuoyu/summarization/models/bertbase/pytorch_model.bin $MODEL_DIR/
 cp /data/zhuoyu/summarization/models/bertbase/vocab.txt  $MODEL_DIR/
-cp /data/zhuoyu/summarization/models/bertbase/config.$EXP_MODEL_NAME.json $MODEL_DIR/config.json
+#cp /data/zhuoyu/summarization/models/bertbase/config.$EXP_MODEL_NAME.json $MODEL_DIR/config.json
+
+cd code/PreSumm/src/prepro
+python change_max_pos.py /data/zhuoyu/summarization/models/bertbase/config.$EXP_MODEL_NAME.json $MODEL_DIR/config.json $MAX_MODEL_POS
+cd ../../../../
 
 #run training
 BERT_OUTPUT_PATH=$OUTPUT_DIR/model_exp_output
@@ -64,7 +72,7 @@ python train.py -task ext -mode train -bert_data_path $BERT_DATA_PATH \
 -ext_dropout 0.1 -model_path $BERT_OUTPUT_PATH \
 -lr 2e-3 -visible_gpus 0,1,2,3 -report_every 50 -save_checkpoint_steps 1000 -batch_size 2000 -train_steps 50000 -accum_count 2 \
 -log_file $BERT_OUTPUT_PATH/logs/train.log -use_interval true -warmup_steps 10000 -max_pos $MAX_POS -dist_init_method "tcp://localhost:10003" \
--model_name bert_lstm -pretrained_name $MODEL_DIR -max_model_pos=$MAX_MODEL_POS -encoder=baseline
+-model_name $ZONG_MODEL -pretrained_name $MODEL_DIR -max_model_pos=$MAX_MODEL_POS -encoder=$ENCODER_TYPE
 cd ../../..
 
 
@@ -81,7 +89,7 @@ python train.py -task ext -mode validate -batch_size 2000 -test_batch_size 2000 
 -sep_optim true -use_interval true -visible_gpus 0 \
 -max_pos $MAX_POS -max_length 200 -alpha 0.95 -min_length 50 \
 -result_path $BERT_OUTPUT_PATH/logs/exp_bert_cnndm.res \
--test_all true -model_name bert_lstm -pretrained_name $MODEL_DIR -max_model_pos=$MAX_MODEL_POS -temp_dir=$ROUGE_TEMP_DIR -encoder=baseline
+-test_all true -model_name $ZONG_MODEL -pretrained_name $MODEL_DIR -max_model_pos=$MAX_MODEL_POS -temp_dir=$ROUGE_TEMP_DIR -encoder=$ENCODER_TYPE
 cd ../../..
 
 
