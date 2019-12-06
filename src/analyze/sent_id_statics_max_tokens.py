1db32ab730b96a081edfc55081bbf863cd2a248c
Zhuoyu Wei <zhuwe@microsoft.com>
2019-12-06 16:37:38 +0800

    add sent id statics max tokens

---
+++ src/analyze/sent_id_statics_max_tokens.py
@@ -0,0 +1,19 @@
+import sys
+import json
+
+id2count={}
+
+with open(sys.argv[1],'r') as f:
+    for line in f:
+        jobj=json.loads(line.strip())
+        labels=jobj['sent_label']
+        for label in labels:
+            if not label in id2count:
+                id2count[label]=0
+            id2count[label]+=1
+
+
+id_counts=sorted(id2count.items(),key=lambda x:x[0])
+for id_count in id_counts:
+    print('{}\t{}'.format(id_count[0],id_count[1]))
+
